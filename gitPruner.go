9404ede5a376bd7a900f562f3e16ec94a05a5092
Ken Tam <ktam33@gmail.com>
2018-03-08 09:38:24 -0600

    initial commit

---
+++ gitPruner.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	path := os.Args[1]
+	os.Chdir(path)
+
+	Info("\nSyncing branches with remote...[git fetch -p]")
+
+	out, err := exec.Command("git", "fetch", "-p").CombinedOutput()
+	if err != nil {
+		fmt.Println("Unable to reach remote. Skipping sync step.\n")
+	} else {
+		fmt.Println(string(out))
+	}
+
+	Info("\nGetting information about local branches...[git branch -vv]")
+	out, err = exec.Command("git", "branch", "-vv").CombinedOutput()
+	checkError(err)
+	branchInfo := string(out)
+	fmt.Println(branchInfo)
+
+	lines := strings.Split(branchInfo, "\n")
+	re := regexp.MustCompile("\\s([^\\s]+?)\\s")
+
+	reader := bufio.NewReader(os.Stdin)
+	foundBranchToDelete := false
+	for _, line := range lines {
+		isMatch, err := regexp.MatchString("\\[.+: gone\\]", line)
+		checkError(err)
+		if isMatch {
+			foundBranchToDelete = true
+			branchName := re.FindStringSubmatch(line)[1]
+			Info("\nBranch \"%s\" is no longer a remote branch. Delete? (y or n)\n", branchName)
+			input, _, err := reader.ReadRune()
+			checkError(err)
+			if input == 'y' || input == 'Y' {
+				Info("Deleting branch...[git branch -d %s]", branchName)
+				out, err = exec.Command("git", "branch", "-d", branchName).CombinedOutput()
+				fmt.Println(string(out))
+			}
+		}
+	}
+	if !foundBranchToDelete {
+		Info("No local branches to prune were found")
+	}
+}
+
+func checkError(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Printf("Error: %s", err)
+	os.Exit(1)
+}
+
+func Info(format string, args ...interface{}) {
+	fmt.Printf("\x1b[34;1m%s\x1b[0m\n", fmt.Sprintf(format, args...))
+}
